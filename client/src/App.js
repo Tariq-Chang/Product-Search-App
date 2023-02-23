b69833286c0fc3aefc30da7455b3a192423d8d1a
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2023-02-23 11:00:57 +0500

    Implemented The Backend Functionality to search the products and added titles to the cards in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,67 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [backendData, setBackendData] = useState([{}]);
+  const [name, setName] = useState(null);
+  const [inputNameValue, setInputNameValue] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("/api/products");
+      const data = response.data;
+      setBackendData(data);
+    }
+    fetchData();
+  }, []);
 
   useEffect(() => {
-    fetch("/api/products")
-      .then((response) => response.json())
-      .then((data) => setBackendData(data));
-  }, []);
+    async function fetchProducts() {
+      if (inputNameValue) {
+        const response = await axios.get(`/api/products/${inputNameValue}`);
+        const data = response.data;
+        setBackendData(data);
+      }
+    }
+    fetchProducts();
+  }, [inputNameValue]);
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputNameValue(name);
+  };
   return (
     <div className="App">
       <div className="navbar">
         <form className="navbar__search">
-          <input type="text" name="product" placeholder="Search Product" />
-          <button type="submit">Search</button>
+          <input
+            type="text"
+            value={name}
+            name="product"
+            placeholder="Search Product"
+            onChange={handleChange}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Search
+          </button>
         </form>
       </div>
       <div className="image__container">
         {backendData.map((product, i) => {
           return (
-            <img
-              key={i}
-              src={product.image}
-              alt={product.name}
-              className="product__image"
-            />
+            <div className="image__wrapper">
+              <p id="product__name">{product.name}</p>
+              <img
+                key={i}
+                src={product.image}
+                alt={product.name}
+                className="product__image"
+              />
+            </div>
           );
         })}
       </div>
--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ app.get("/api/products", (req, res) => {
   res.status(200).json(products);
 });
 
+app.get("/api/products/:name", (req, res) => {
+  const name = req.params.name;
+
+  if (name) {
+    // const result = products.filter((product) => product.name.startsWith(name));
+    const result = products.filter((product) => product.name.includes(name));
+
+    return res.status(200).json(result);
+  }
+  return res.status(404).json({ message: "Could not find the product" });
+});
+
 app.listen(5000, () => {
   console.log("Server is listening at port 5000...");
 });
